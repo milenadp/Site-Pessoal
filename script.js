da4c1820b12eb6df425b671d1eb04cd32c5a82b1
Milena <milenadiasdpz@gmail.com>
2020-08-31 10:08:49 -0300

    Início desenvolvimento do Site - falta adicionar requisitos JS

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function() {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
+
+function apagar() {
+    document.querySelector("#preco").innerHTML = `R$ 0.00`
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo:`
+}
+
+function dataRodape() {
+    data = new Date();
+    document.getElementById('data').value = data.getDay() + '/' + data.getMonth() + '/' + data.getFullYear();
+}
